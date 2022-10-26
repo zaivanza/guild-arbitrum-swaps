3b6b7e05bb5a4fadb82571c8da1a929e1e265896
hodlmod.eth <93076644+zaivanza@users.noreply.github.com>
2022-10-26 08:20:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -398,7 +398,7 @@ if __name__ == "__main__":
 
     cprint(f'\nsubscribe to us : https://t.me/hodlmodeth', 'magenta')
     
-    with open("arbitrum/private_keys.txt", "r") as f:
+    with open("private_keys.txt", "r") as f:
         keys_list = [row.strip() for row in f]
 
 
